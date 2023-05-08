e510abc19ec70936693ddcc0cd6de480099feb35
Eason <eason.liao.1994@gmail.com>
2023-05-08 23:46:18 +0800

    Fix issue: Hide Django security key in django_central_wallet application
    I apologize for the oversight of exposing the Django security key in the django_central_wallet application. This commit addresses the issue by taking the following steps:
    
    - Removed the exposed security key from the repository
    - Generated a new secure key using django.core.management.utils.get_random_secret_key()
    - Updated the Django settings file to use the new secure key from `.env` file
    - Reviewed the repository for any other accidentally exposed sensitive information and addressed them
    
    I apologize for any inconvenience caused by this oversight. I appreciate the report and have taken immediate action to rectify the situation. Thank you for bringing this to my attention.
    
    Fixes #1

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -U -r requirements.txt
+cp .env.default .env
 cp django_central_wallet/settings.py.default django_central_wallet/settings.py
 cp db.sqlite3.default db.sqlite3
 # python3 manage.py collectstatic
