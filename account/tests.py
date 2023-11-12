af2aba3eddb928faae97f21ee2036b40ebd80faf
Eason <eason.liao.1994@gmail.com>
2023-11-12 16:12:02 +0800

    update test case for accounts

--- account/tests.py
+++ account/tests.py
@@ -1,30 +1,33 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.core import mail
+from account.tokens import EmailVerifyTokenGenerator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 import json
 
-from django.urls import reverse
-from rest_framework.test import APITestCase
-from django.contrib.auth.models import User
-from rest_framework import status
-from .models import *
+User = get_user_model()
 
-class UserAPITest(APITestCase):
+class UserTestCase(TestCase):
     def setUp(self):
-        self.user1 = User.objects.create_user('user1@test.com', 'Test123a')
-        self.assertEqual(self.user1.is_verified, False)
+        self.user1 = User.objects.create_user(email='user1@test.com', password='password')
+        self.user1.is_active = False
+        self.user1.save()
+
+    def test_activate_user(self):
         self.user1.activate('email')
         self.assertEqual(self.user1.is_verified, True)
-        
         self.assertEqual(User.objects.count(), 1)
-    
-    def test_register_api(self):
-        # url setup
-        register_api = reverse('register')
 
+    def test_register_api(self):
+        register_api = reverse('register')
         data = {
             "email": 'test@test.com',
             "first_name": "test_first_name",
             "last_name": "test_last_name",
             "password": "testpassword"
-            }
+        }
         response = self.client.post(register_api, data, format='json')
         response_data = json.loads(response.content)
 
@@ -32,7 +35,7 @@ class UserAPITest(APITestCase):
         self.assertEqual(response_data['email'], data['email'])
         self.assertEqual(response_data['first_name'], data['first_name'])
         self.assertEqual(response_data['last_name'], data['last_name'])
-        
+
         register_user = User.objects.get(email=data['email'])
         self.assertEqual(register_user.is_active, False)
         self.assertEqual(register_user.is_verified, False)
@@ -41,3 +44,15 @@ class UserAPITest(APITestCase):
         register_user.activate('email')
         self.assertEqual(register_user.is_active, True)
         self.assertEqual(register_user.is_verified, True)
+
+    def test_activate_view(self):
+        token_generator = EmailVerifyTokenGenerator()
+        token = token_generator.make_token(self.user1)
+        uid = urlsafe_base64_encode(force_bytes(self.user1.pk))
+        activate_url = reverse('activate', kwargs={'uidb64': uid, 'token': token})
+
+        response = self.client.get(activate_url)
+        self.user1.refresh_from_db()
+        self.assertEqual(self.user1.is_verified, True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mail_verified.html')
